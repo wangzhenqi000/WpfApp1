847952533d4e4e375191b0585c49b6c4010693cb
wangzhenqi000 <13069878636@163.com>
2018-10-30 15:48:01 +0800

    添加项目文件。

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double result = 0.0;
+            button.IsEnabled = false;
+            Console.WriteLine(DateTime.Now.ToShortTimeString());
+            for (int i = 0; i < 1000000; i++)
+            {
+                result += Math.Cos(i);
+                result *= Math.Sin(i);
+            }
+            Console.WriteLine(DateTime.Now.ToShortTimeString());
+            button.IsEnabled = true;
+        }
+    }
+}
